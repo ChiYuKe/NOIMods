d312b61e9546a45693203364f662eabb4edfe02f
liukele <2902599249@qq.com>
2024-08-10 20:58:28 +0800

    添加字符串

--- DebuffRoulette/KModPatch.cs
+++ DebuffRoulette/KModPatch.cs
@@ -1,10 +1,12 @@
 ﻿using HarmonyLib;
 using KMod;
+using KModTool;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace DebuffRoulette
 {
@@ -36,6 +38,34 @@ namespace DebuffRoulette
         }
     }
 
+    [HarmonyPatch(typeof(EntityConfigManager), "LoadGeneratedEntities")]
+    public class New_Plant_Database_Description
+    {
+        public static void Prefix()
+        {
+            KModStringUtils.Add_New_Deaths_Strings("KModCombat", STRINGS.DUPLICANTS.DEATHS.NAME, STRINGS.DUPLICANTS.DEATHS.DESCRIPTION);
+
+
+        }
+    }
+
+
+
+    //[HarmonyPatch(typeof(MinionPersonalityPanel), "RefreshBioPanel")]
+    //public static class RefreshBioPanelPatch
+    //{
+
+    //    // 使用 Postfix 在原方法执行后插入代码
+    //    public static void Postfix(CollapsibleDetailContentPanel targetPanel, GameObject targetEntity)
+    //    {
+    //        MinionIdentity minionIdentity = targetEntity.GetComponent<MinionIdentity>();
+    //        float currentCycle = (float)GameClock.Instance.GetCycle();
+    //        float minionAge = (currentCycle - minionIdentity.arrivalTime) * 600f;
+    //        float num1  = 17 * 600f - minionAge;
+    //        // 插入新的标签
+    //        targetPanel.SetLabel("zhuangtai：", "衰老死亡时间：" + num1, "");
+    //    }
+    //}
 
 
 
