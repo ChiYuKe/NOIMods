fe70b9d9f840dfa9db6f0b472fb20f994d52da57
liukele <2902599249@qq.com>
2024-08-15 02:04:24 +0800

    调整文件结构

--- DebuffRoulette/Buff.cs
+++ DebuffRoulette/Buff.cs
@@ -1,10 +1,13 @@
 ﻿using Database;
 using KModTool;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace DebuffRoulette
 {
    
-    internal class Buff
+    internal class DeBuff
     {
        
         public static void Register(ModifierSet parent)
@@ -51,5 +54,56 @@ namespace DebuffRoulette
 
 
         }
+
+
+        public static void ApplyRandomDebuff(HashSet<GameObject> cachedMinionGameObjects)
+        {
+            List<string> debuffTypes = new List<string> { "debuff1", "debuff2", "debuff3", "debuff4" };
+            int minionCount = cachedMinionGameObjects.Count;
+            int numToSelect = 3;
+
+            if (minionCount >= numToSelect)
+            {
+                // 打乱小人列表，随机选择前 numToSelect 个小人
+                List<GameObject> selectedMinions = cachedMinionGameObjects.OrderBy(x => UnityEngine.Random.value).Take(numToSelect).ToList();
+
+                foreach (GameObject gameObject in selectedMinions)
+                {
+                    if (gameObject == null) continue;
+
+                    Klei.AI.Effects effectsComponent = gameObject.GetComponent<Klei.AI.Effects>();
+                    if (effectsComponent == null) continue;
+
+                    // 随机整一个 Debuff给小人
+                    string randomDebuff = debuffTypes[UnityEngine.Random.Range(0, debuffTypes.Count)];
+
+                    if (!effectsComponent.HasEffect(randomDebuff))
+                    {
+                        effectsComponent.Add(randomDebuff, true);
+                        NotifyDebuffApplied1(gameObject); // 通知玩家谁被添加了DeBuff
+
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("复制人数不足3人，不添加 Debuff。");
+            }
+        }
+
+
+        private static void NotifyDebuffApplied1(GameObject gameObject)
+        {
+            Notifier notifier = gameObject.AddOrGet<Notifier>();
+            Notification notification = new Notification(
+                DebuffRoulette.STRINGS.MISC.NOTIFICATIONS.DEBUFFROULETTE.NAME,
+                NotificationType.MessageImportant,
+                (notificationList, data) => notificationList.ReduceMessages(false),
+                "/t• " + gameObject.GetProperName(), true, 0f, null, null, null, true, false, false
+            );
+            notifier.Add(notification, "");
+        }
+
+
     }
 }
